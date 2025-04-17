53c4aa02e27eafb33af741847f1a069bba5fd72b
Yoojin Park <yoojin.park@edu.sait.ca>
2025-04-17 13:52:05 -0600

    Initial commit : Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+
+function App() {
+
+
+  return (
+    <>
+
+       
+        <h1 className="text-5xl underline font-bold text-pink-500">Hello YoojinLab!</h1>
+
+    </>
+  )
+}
+
+export default App
